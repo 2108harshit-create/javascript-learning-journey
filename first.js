6c48ebb16e457771728972a9038a81993d89c016
Harshit <2108harshit@gmail.com>
2026-01-10 20:51:03 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,18 @@
+const accountnum = 12345
+var accountId = "23412"
+let password = "343434"
+accountname = "Harshit"
+
+// accountId=21 not allowed
+// console.log(accountnum);
+
+accountId = "99999"
+password = "99999"
+accountname = "Ankit"
+// console.log(accountId);
+// console.log(password);
+// console.log(accountname);   
+
+console.table([accountname,accountId,password,accountnum])
+
+// dont use var 
