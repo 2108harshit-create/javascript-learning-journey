55e1c0c1b30966e3c478dab7b04600e4cd9e6c2a
Harshit <2108harshit@gmail.com>
2026-01-11 19:35:47 +0530

    Add files via upload

---
+++ conversionoperation.js
@@ -0,0 +1,18 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber = String(score);  //if we use string it will convert null to "null" or if we use number it will convert null to 0
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+let score2 = 33
+
+let stringscore = String(score2)
+console.log(typeof stringscore);
+console.log(stringscore);
+
+
+
+
