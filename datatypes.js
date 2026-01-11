6710d6c8676828074a9fc194c7b4e3547fbf9de5
Harshit <2108harshit@gmail.com>
2026-01-11 12:38:10 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+// number 
+// String
+// Boolean= True False
+// bigint
+// null
+// undefined
+// symbol
+// object   
+
+
+let age = 45;
+let name = "Harshit";
+let reqaccept = true;    
+let id = 1234567890123456789012345678901234567890n;
+let req = null;
+
+
+console.table([age,name,reqaccept,id,req])
